8d859b6fcb2452d0a849e21d9b9456c645c15e49
khoado <khoadonguyen8@gmail.com>
2025-01-05 17:59:49 +0700

    --

---
+++ src/main/java/com/pc/mall/common/Log/WebLogAspect.java
@@ -0,0 +1,7 @@
+package com.pc.mall.common.Log;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebLogAspect {
+}
--- src/main/java/com/pc/mall/common/Util/RequestUtil.java
+++ src/main/java/com/pc/mall/common/Util/RequestUtil.java
@@ -1,4 +1,38 @@
 package com.pc.mall.common.Util;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class RequestUtil {
-}
+    public static String getRequestIp(HttpServletRequest request) {
+        // lấy ip address từ client khi truy cập qua api
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-client-ip");
+
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-Ip");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getLocalAddr();
+            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
+                InetAddress inetAddress = null;
+                try {
+                    inetAddress = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    throw new RuntimeException(e);
+                }
+                ipAddress = inetAddress.getHostAddress();
+
+            }
+        }
+        if(ipAddress.length()>15&& ipAddress!=null){
+            if(ipAddress.indexOf(",")>0){
+                ipAddress =ipAddress.substring(0,ipAddress.indexOf(","));
+            }
+        }
+        return  ipAddress;
+    }}
